b81048092ecf7e80cacb7ca03fc24cb0fb09aaf4
rahulsingodia <148620072+rahulsingodia@users.noreply.github.com>
2025-05-28 17:23:41 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Date currentDate = new Date();
-        System.out.println("Hello, Sir: ");
+        System.out.println("Hello, Docker! Current date: " + currentDate);
     }
 }
